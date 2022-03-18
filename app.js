f3527c05711367cd74870094ef3b445039e908ec
spaasie <68701772+spaasie@users.noreply.github.com>
2022-03-18 12:06:15 +0200

    add etch function and etched class with pointerenter event listener

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const container = document.querySelector(".container");
 
+function etch() {
+  this.classList.add("etched");
+}
 
-for (let i=256; i >= 0; i--) {
+for (let i=256; i > 0; i--) {
   const div = document.createElement("div");
+  div.addEventListener("pointerenter", etch);
   container.appendChild(div);
 }
